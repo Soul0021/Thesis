6f8b0af9b058d8ede43e5c3e22f7d546a4235d9e
Soul0021 <malacai2100@gmail.com>
2024-10-09 20:44:15 +0800

    Images now show up in their places Updated staticfiles

--- Thesis/urls.py
+++ Thesis/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -26,7 +27,7 @@ urlpatterns = [
     path ("",include("userauths.urls")),
     
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, documents_root=settings.STATIC_ROOT)
